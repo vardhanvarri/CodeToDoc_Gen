ed5f7a08ee05a4b4aa459e263669f9f009715aca
Shyam Sri Vardhan Varri <165015981+vardhanvarri@users.noreply.github.com>
2026-06-02 12:00:09 +0530

    Update app.run configuration for deployment

--- app.py
+++ app.py
@@ -55,4 +55,8 @@ def gitlab_webhook():
 application = app
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
